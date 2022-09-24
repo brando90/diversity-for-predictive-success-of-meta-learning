5d8de17777d7720d87171947f0b2ed954fed921c
Brando Miranda <brandojazz@gmail.com>
2022-09-24 13:02:39 -0700

    make sure you move the folder but it doesn't already exists or it will copy it inside the folder it already exists. To try to avoid mv to do that issue I removed repetition of the name at the end and just do path/ with / at the end to make it clear. idk what happens if its already there.

---
+++ move_data_and_conda_to_shared_rsaas.sh
@@ -0,0 +1,13 @@
+##ln -s file1 link1
+
+mv ~/data /shared/rsaas/miranda9/
+ln -s /shared/rsaas/miranda9/data ~/data
+
+mv ~/miniconda3 /shared/rsaas/miranda9/
+ln -s /shared/rsaas/miranda9/miniconda3 ~/miniconda3
+
+mv ~/data_folder_fall2020_spring2021 /shared/rsaas/miranda9/
+ln -s /shared/rsaas/miranda9/data_folder_fall2020_spring2021 ~/data_folder_fall2020_spring2021
+
+#mv ~/.opam /shared/rsaas/miranda9/.opam
+#ln -s /shared/rsaas/miranda9/.opam ~/.opam
