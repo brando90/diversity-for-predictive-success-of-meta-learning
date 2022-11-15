248c750ac292022595fb7e9210eb205d0dfb76b7
Brando Miranda <brandojazz@gmail.com>
2022-11-15 12:49:31 -0800

    looping through my delauny worked without issues

--- main_nohup_snap.sh
+++ main_nohup_snap.sh
@@ -1,10 +1,15 @@
 # https://unix.stackexchange.com/questions/724902/how-does-one-send-new-commands-to-run-to-an-already-running-nohup-process-e-g-r
 # sh ~/diversity-for-predictive-success-of-meta-learning/main_nohup_snap.sh
 # - set up this main sh script
+export RUN_PWD=$(pwd)
 source ~/.bashrc
 source ~/.bash_profile
 source ~/.bashrc.user
 echo HOME = $HOME
+# since snap .bash.user cd's me into HOME at dfs
+cd $RUN_PWD
+echo RUN_PWD = $RUN_PWD
+realpath .
 
 source cuda11.1
 
@@ -17,8 +22,10 @@ echo SLURM_JOBID = $SLURM_JOBID
 export OUT_FILE=$PWD/main.sh.o$SLURM_JOBID
 export ERR_FILE=$PWD/main.sh.e$SLURM_JOBID
 export WANDB_DIR=$HOME/wandb_dir
+echo $OUT_FILE
+echo $ERR_FILE
 
-export CUDA_VISIBLE_DEVICES=4
+export CUDA_VISIBLE_DEVICES=5
 echo CUDA_VISIBLE_DEVICES = $CUDA_VISIBLE_DEVICES
 python -c "import torch; print(torch.cuda.get_device_name(0));"
 
@@ -29,14 +36,13 @@ python -c "import torch; print(torch.cuda.get_device_name(0));"
 # - expt python script then inside that python pid attach a reauth process
 # should I run rauth within python with subprocess or package both the nohup command and the rauth together in badsh somehow
 #python -u ~/diversity-for-predictive-success-of-meta-learning/div_src/diversity_src/experiment_mains/main_sl_with_ddp.py --manual_loads_name sl_hdb1_5cnn_adam_cl_filter_size --filter_size 4 > $OUT_FILE 2> $ERR_FILE &
-
-#nohup sh -c 'echo $SU_PASSWORD | /afs/cs/software/bin/reauth; python -u ~/diversity-for-predictive-success-of-meta-learning/div_src/diversity_src/experiment_mains/main_sl_with_ddp.py --manual_loads_name sl_hdb1_5cnn_adam_cl_filter_size --filter_size 4 > $OUT_FILE 2> $ERR_FILE' > nohup.out$SLURM_JOBID &
-
-nohup python -u ~/diversity-for-predictive-success-of-meta-learning/div_src/diversity_src/experiment_mains/main_sl_with_ddp.py --manual_loads_name sl_hdb1_5cnn_adam_cl_filter_size --filter_size 4 > $OUT_FILE 2> $ERR_FILE &
+nohup sh -c 'echo $SU_PASSWORD | /afs/cs/software/bin/reauth; python -u ~/diversity-for-predictive-success-of-meta-learning/div_src/diversity_src/experiment_mains/main_sl_with_ddp.py --manual_loads_name sl_hdb1_5cnn_adam_cl_filter_size --filter_size 4 > $OUT_FILE 2> $ERR_FILE' > $PWD/nohup.out$SLURM_JOBID &
+#nohup python -u ~/diversity-for-predictive-success-of-meta-learning/div_src/diversity_src/experiment_mains/main_sl_with_ddp.py --manual_loads_name sl_hdb1_5cnn_adam_cl_filter_size --filter_size 4 > $OUT_FILE 2> $ERR_FILE &
 
 # other option is to run `echo $SU_PASSWORD | /afs/cs/software/bin/reauth` inside of python, right?
 export JOB_PID=$!
 echo JOB_PID = $JOB_PID
+echo SLURM_JOBID = $SLURM_JOBID
 
 # - Done
-echo "Done with the dispatching (daemon) sh script
+echo "Done with the dispatching (daemon) sh script"
