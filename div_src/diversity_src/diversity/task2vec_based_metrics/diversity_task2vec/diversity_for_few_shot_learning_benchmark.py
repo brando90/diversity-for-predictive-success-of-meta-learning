fixed imports so div computation works

div_src/diversity_src/diversity/task2vec_based_metrics/diversity_task2vec/diversity_for_few_shot_learning_benchmark.py
@@ -65,12 +65,13 @@ from learn2learn.vision.benchmarks import BenchmarkTasksets
 from torch import nn, Tensor
 from torch.utils.data import Dataset
 
-import task2vec
-import task_similarity
+import diversity_src.diversity.task2vec_based_metrics.task2vec as task2vec
+import diversity_src.diversity.task2vec_based_metrics.task_similarity as task_similarity
 from dataset import TaskDataset
-from models import get_model
-from task2vec import Embedding, Task2Vec, ProbeNetwork
-
+# from models import get_model
+# from task2vec import Embedding, Task2Vec, ProbeNetwork
+from diversity_src.diversity.task2vec_based_metrics.models import get_model
+from diversity_src.diversity.task2vec_based_metrics.task2vec import Embedding, Task2Vec, ProbeNetwork
 
 # - probe network code
 

div_src/diversity_src/diversity/task2vec_based_metrics/models.py
@@ -17,7 +17,8 @@ import torch.utils.model_zoo as model_zoo
 import torchvision.models.resnet as resnet
 import torch
 
-from task2vec import ProbeNetwork
+# from task2vec import ProbeNetwork
+from diversity_src.diversity.task2vec_based_metrics.task2vec import ProbeNetwork
 
 _MODELS = {}
 