2009603dff87ec7b528194e21536a50779a6b8e9
Brando Miranda <brandojazz@gmail.com>
2023-01-06 14:35:02 -0600

    gsutil attempt to download mscoco. Also imgenet ilsvrc_2012 doesnt work yet

--- download_mi_l2l.py
+++ download_mi_l2l.py
@@ -6,6 +6,9 @@ exist. Idk what the solution is or why it ALWAYS wants to redownload it.
 
 
 def get_download_mi_l2l_now():
+    """
+    url of upload on zenodo: https://zenodo.org/deposit?page=1&size=20
+    """
     root = '~/data/l2l_data/',
     url: str = 'https://zenodo.org/record/7311663/files/brandoslearn2learnminiimagenet.zip'
     from uutils import download_and_extract
