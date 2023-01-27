f477c6f8af3a6692d80d3a16d673e379de10f296
Brando Miranda <brandojazz@gmail.com>
2023-01-26 16:15:20 -0800

    removed the careless/lazy mistake of not making deep copies but that copies the model so its too expensive though correct, solution was to create a new meta learning object (agent) and assign the new learning rates there to avoid re-writing the old meta-learning object. Careful with mutation. Ok the bug needed to be fix at the meta-learner class since it didn't have that int being set. The deepcopy of an int is unneccessary but it will work. The issue was that the object was re-setting the args value which resetted everything globally. By setting it as a field that was only for that obj and not resetting the global args, it removed the issue. This is the issue with globals!

--- div_src/diversity_src/data_analysis/stats_analysis_with_emphasis_on_effect_size.py
+++ div_src/diversity_src/data_analysis/stats_analysis_with_emphasis_on_effect_size.py
@@ -2,6 +2,7 @@
 Stistical analysis to detect difference, emphasizing on effect size.
 Since it meta-learning, the errors/accs are known to have large stds.
 """
+import sys
 from argparse import Namespace
 
 import torch
@@ -14,7 +15,10 @@ from uutils.torch_uu.meta_learners.maml_meta_learner import MAMLMetaLearner
 from uutils.torch_uu import norm
 
 
-def stats_analysis_with_emphasis_on_effect_size(args: Namespace):
+def stats_analysis_with_emphasis_on_effect_size(args: Namespace,
+                                                perform_full_performance_comparison: bool = False,
+                                                save_hist_losses: bool = True,
+                                                ):
     """
     Note:
         - you need to make sure the models rand, sl, maml are loaded correctly before using this function.
@@ -28,6 +32,104 @@ def stats_analysis_with_emphasis_on_effect_size(args: Namespace):
         - H0: no diff btw usl and maml
         - H1: diff btw usl and maml (& USL > MAML, chekcing populat belief)
     """
+    # _debug(args)
+    # results: dict = {}
+    # original_lr_inner = args.meta_learner.lr_inner
+    # print(f'{original_lr_inner=}')
+    #
+    # print('\n---- test 1: only using print_performance_4_maml() ----')
+    # print_performance_4_maml(args, args.mdl_maml, 5, original_lr_inner, debug_print=True)
+    # print_performance_4_maml(args, args.mdl_maml, 0, 0, debug_print=True)
+    # print_performance_4_maml(args, args.mdl_maml, 5, original_lr_inner, debug_print=True)
+    #
+    # print('\n---- test 2: basic_sanity_checks_maml0_does_nothing ----')
+    # from diversity_src.data_analysis.common import basic_sanity_checks_maml0_does_nothing
+    # print_performance_4_maml(args, args.mdl_maml, 5, original_lr_inner, debug_print=True)
+    # basic_sanity_checks_maml0_does_nothing(args)
+    # print_performance_4_maml(args, args.mdl_maml, 5, original_lr_inner, debug_print=True)
+    #
+    # print('\n---- test 3: get_accs_losses_all_splits_maml ----')
+    # loaders: dict = args.dataloaders
+    # from diversity_src.data_analysis.common import get_accs_losses_all_splits_maml
+    # from diversity_src.data_analysis.common import get_accs_losses_all_splits_usl
+    # from diversity_src.data_analysis.common import get_mean_and_ci_from_results
+    # print_performance_4_maml(args, args.mdl_maml, 5, original_lr_inner, debug_print=True)
+    # basic_sanity_checks_maml0_does_nothing(args)
+    # print_performance_4_maml(args, args.mdl_maml, 5, original_lr_inner, debug_print=True)
+    # results_maml5 = get_accs_losses_all_splits_maml(args, args.mdl_maml, loaders, 5, original_lr_inner)
+    # loss, loss_ci, acc, acc_ci = get_mean_and_ci_from_results(results_maml5, 'train')
+    # print(f'--> train: {(loss, loss_ci, acc, acc_ci)=}')
+    # results_maml10 = get_accs_losses_all_splits_maml(args, args.mdl_maml, loaders, 10, original_lr_inner)
+    # loss, loss_ci, acc, acc_ci = get_mean_and_ci_from_results(results_maml10, 'train')
+    # print(f'--> train: {(loss, loss_ci, acc, acc_ci)=}')
+    # results_usl = get_accs_losses_all_splits_usl(args, args.mdl_sl, loaders)
+    # loss, loss_ci, acc, acc_ci = get_mean_and_ci_from_results(results_usl, 'train')
+    # print(f'--> train: {(loss, loss_ci, acc, acc_ci)=}')
+    # print_performance_4_maml(args, args.mdl_maml, 5, original_lr_inner, debug_print=True)
+    #
+    # print('\n---- test 4: does printing results later screw things up?  ----')
+    # print('---- Sanity check: meta-train acc & loss for each method (usl & maml) values at the end of training ----')
+    # from diversity_src.data_analysis.common import get_mean_and_ci_from_results
+    # loss, loss_ci, acc, acc_ci = get_mean_and_ci_from_results(results_maml5, 'train')
+    # print(f'train: {(loss, loss_ci, acc, acc_ci)=}')
+    # results['train_maml5'] = (loss, loss_ci, acc, acc_ci)
+    # loss, loss_ci, acc, acc_ci = get_mean_and_ci_from_results(results_maml10, 'train')
+    # print(f'train: {(loss, loss_ci, acc, acc_ci)=}')
+    # results['train_maml10'] = (loss, loss_ci, acc, acc_ci)
+    # loss, loss_ci, acc, acc_ci = get_mean_and_ci_from_results(results_usl, 'train')
+    # print(f'train: {(loss, loss_ci, acc, acc_ci)=}')
+    # results['train_usl'] = (loss, loss_ci, acc, acc_ci)
+    # print_performance_4_maml(args, args.mdl_maml, 5, original_lr_inner, debug_print=True)
+
+    # # -- Start code for real
+    # print('\n---------  Start code for real  ---------')
+    # original_lr_inner = args.meta_learner.lr_inner
+    # print(f'{original_lr_inner=}')
+    # # - get datalaoders from args
+    # loaders: dict = args.dataloaders
+    # # - results dict
+    # results: dict = {}
+    # # -- Guard: that models maml != usl != rand, i.e. models were loaded correctly before using this function
+    # basic_guards_that_maml_usl_and_rand_models_loaded_are_different(args)
+    # assert norm(args.mdl_rand) != norm(args.mdl_maml) != norm(args.mdl_sl), f'You need to load these before using this.'
+    #
+    # # -- Sanity check: basic checks that meta-train errors & loss are fine (check not to different from final learning curve vals)
+    # from diversity_src.data_analysis.common import basic_sanity_checks_maml0_does_nothing
+    # print_performance_4_maml(args, args.mdl_maml, 5, original_lr_inner, debug_print=True)
+    # basic_sanity_checks_maml0_does_nothing(args)
+    # print_performance_4_maml(args, args.mdl_maml, 5, original_lr_inner, debug_print=True)
+    #
+    # # -- Get the losses & accs for each method (usl & maml) for all splits & save them
+    # # - once the model guard has been passed you should be able to get args.mdl_rand, args.mdl_maml, args.mdl_sl safely
+    # original_lr_inner = args.meta_learner.lr_inner
+    # print(f'{original_lr_inner=}')
+    # from diversity_src.data_analysis.common import get_accs_losses_all_splits_maml
+    # results_maml5 = get_accs_losses_all_splits_maml(args, args.mdl_maml, loaders, 5, original_lr_inner)
+    # results_maml10 = get_accs_losses_all_splits_maml(args, args.mdl_maml, loaders, 10, original_lr_inner)
+    # from diversity_src.data_analysis.common import get_accs_losses_all_splits_usl
+    # results_usl = get_accs_losses_all_splits_usl(args, args.mdl_sl, loaders)
+    #
+    # print_performance_4_maml(args, args.mdl_maml, 5, original_lr_inner, debug_print=True)
+    # basic_sanity_checks_maml0_does_nothing(args)
+    # print_performance_4_maml(args, args.mdl_maml, 5, original_lr_inner, debug_print=True)
+    #
+    # from diversity_src.data_analysis.common import get_mean_and_ci_from_results
+    # loss, loss_ci, acc, acc_ci = get_mean_and_ci_from_results(results_maml5, 'train')
+    # print(f'train: {(loss, loss_ci, acc, acc_ci)=}')
+    # results['train_maml5'] = (loss, loss_ci, acc, acc_ci)
+    # loss, loss_ci, acc, acc_ci = get_mean_and_ci_from_results(results_maml10, 'train')
+    # print(f'train: {(loss, loss_ci, acc, acc_ci)=}')
+    # results['train_maml10'] = (loss, loss_ci, acc, acc_ci)
+    # loss, loss_ci, acc, acc_ci = get_mean_and_ci_from_results(results_usl, 'train')
+    # print(f'train: {(loss, loss_ci, acc, acc_ci)=}')
+    # results['train_usl'] = (loss, loss_ci, acc, acc_ci)
+    #
+    # print_performance_4_maml(args, args.mdl_maml, 5, original_lr_inner, debug_print=True)
+    # basic_sanity_checks_maml0_does_nothing(args)
+    # print_performance_4_maml(args, args.mdl_maml, 5, original_lr_inner, debug_print=True)
+
+    # -- Start code for real
+    print('\n---------------------  Start code for real2  ---------------------')
     # - get datalaoders from args
     loaders: dict = args.dataloaders
     # - results dict
@@ -38,7 +140,7 @@ def stats_analysis_with_emphasis_on_effect_size(args: Namespace):
 
     # -- Sanity check: basic checks that meta-train errors & loss are fine (check not to different from final learning curve vals)
     from diversity_src.data_analysis.common import basic_sanity_checks_maml0_does_nothing
-    basic_sanity_checks_maml0_does_nothing(args)
+    basic_sanity_checks_maml0_does_nothing(args, loaders)
 
     # -- Get the losses & accs for each method (usl & maml) for all splits & save them
     # - once the model guard has been passed you should be able to get args.mdl_rand, args.mdl_maml, args.mdl_sl safely
@@ -47,9 +149,11 @@ def stats_analysis_with_emphasis_on_effect_size(args: Namespace):
     from diversity_src.data_analysis.common import get_accs_losses_all_splits_maml
     results_maml5 = get_accs_losses_all_splits_maml(args, args.mdl_maml, loaders, 5, original_lr_inner)
     results_maml10 = get_accs_losses_all_splits_maml(args, args.mdl_maml, loaders, 10, original_lr_inner)
-    print_performance_4_maml(args, args.mdl_maml, 5, original_lr_inner)
     from diversity_src.data_analysis.common import get_accs_losses_all_splits_usl
     results_usl = get_accs_losses_all_splits_usl(args, args.mdl_sl, loaders)
+    print_performance_4_maml(args, args.mdl_maml, loaders, 5, original_lr_inner, debug_print=True)
+
+    sys.exit()
 
     # -- Sanity check: meta-train acc & loss for each method (usl & maml) are close to final loss after training
     print('---- Sanity check: meta-train acc & loss for each method (usl & maml) values at the end of training ----')
@@ -84,13 +188,69 @@ def stats_analysis_with_emphasis_on_effect_size(args: Namespace):
         args.alpha, print_groups_data=True)
     results['maml10_vs_usl'] = (cohen_d, standardized_acceptable_difference1, standardized_acceptable_difference2)
 
-    # -- save results
+    # -- Save hist losses
+    if save_hist_losses:
+        pass
+    # -- Save results
     results['results_maml5'] = results_maml5
     results['results_maml10'] = results_maml10
     results['results_usl'] = results_usl
     torch.save(results, args.log_root / f'results.pt')
     save_to_json_pretty(results, args.log_root / f'results.json')
     save_args(args)
-    # -- do performan comparison
-    # todo
+    # -- do perform comparison
+    if perform_full_performance_comparison:
+        print('---- Full performance comparison ----')
+        from diversity_src.data_analysis.common import comparison_via_performance
+        # comparison_via_performance(args)
+        print_performance_4_maml(args, args.mdl_maml, 5, original_lr_inner, debug_print=True)
     return results
+
+
+def _debug(args: Namespace):
+    print('\n---- Start Debug ----')
+    from diversity_src.data_analysis.common import basic_sanity_checks_maml0_does_nothing
+
+    from diversity_src.data_analysis.common import get_accs_losses_all_splits_maml
+    from diversity_src.data_analysis.common import get_accs_losses_all_splits_usl
+
+    from diversity_src.data_analysis.common import get_mean_and_ci_from_results
+
+    results: dict = {}
+    original_lr_inner = args.meta_learner.lr_inner
+    print(f'{original_lr_inner=}')
+    loaders: dict = args.dataloaders
+
+    basic_guards_that_maml_usl_and_rand_models_loaded_are_different(args)
+
+    print()
+    print_performance_4_maml(args, args.mdl_maml, loaders, 5, original_lr_inner, debug_print=True)
+    basic_sanity_checks_maml0_does_nothing(args, loaders)
+    print_performance_4_maml(args, args.mdl_maml, loaders, 5, original_lr_inner, debug_print=True)
+
+    results_maml5 = get_accs_losses_all_splits_maml(args, args.mdl_maml, loaders, 5, original_lr_inner)
+    results_maml10 = get_accs_losses_all_splits_maml(args, args.mdl_maml, loaders, 10, original_lr_inner)
+    results_usl = get_accs_losses_all_splits_usl(args, args.mdl_sl, loaders)
+
+    print()
+    print_performance_4_maml(args, args.mdl_maml, loaders, 5, original_lr_inner, debug_print=True)
+    basic_sanity_checks_maml0_does_nothing(args, loaders)
+    print_performance_4_maml(args, args.mdl_maml, loaders, 5, original_lr_inner, debug_print=True)
+
+    print()
+    loss, loss_ci, acc, acc_ci = get_mean_and_ci_from_results(results_maml5, 'train')
+    print(f'----> train: {(loss, loss_ci, acc, acc_ci)=}')
+    results['train_maml5'] = (loss, loss_ci, acc, acc_ci)
+    loss, loss_ci, acc, acc_ci = get_mean_and_ci_from_results(results_maml10, 'train')
+    print(f'----> train: {(loss, loss_ci, acc, acc_ci)=}')
+    results['train_maml10'] = (loss, loss_ci, acc, acc_ci)
+    loss, loss_ci, acc, acc_ci = get_mean_and_ci_from_results(results_usl, 'train')
+    print(f'----> train: {(loss, loss_ci, acc, acc_ci)=}')
+    results['train_usl'] = (loss, loss_ci, acc, acc_ci)
+
+    print()
+    print_performance_4_maml(args, args.mdl_maml, loaders, 5, original_lr_inner, debug_print=True)
+    basic_sanity_checks_maml0_does_nothing(args, loaders)
+    print_performance_4_maml(args, args.mdl_maml, loaders, 5, original_lr_inner, debug_print=True)
+    print('---- End Debug ----\n')
+
