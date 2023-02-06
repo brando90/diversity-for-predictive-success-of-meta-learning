298daf264c4b266a7d197915b079795d42f0ce7c
Brando Miranda <brandojazz@gmail.com>
2023-02-06 14:36:11 -0800

    relabaling and saving to a file works for hdb4, testing hdb1 and also saving labels to speed up things I hope

--- div_src/diversity_src/dataloaders/usl/hdb1_mi_omniglot_usl_dl.py
+++ div_src/diversity_src/dataloaders/usl/hdb1_mi_omniglot_usl_dl.py
@@ -98,11 +98,16 @@ def loop_through_usl_hdb1_and_pass_data_through_mdl():
 
     # - loop through tasks
     device = torch.device(f"cuda:{0}" if torch.cuda.is_available() else "cpu")
-    from models import get_model
     # model = get_model('resnet18', pretrained=False, num_classes=n_train_cls).to(device)
     # model = get_model('resnet18', pretrained=True, num_classes=n_train_cls).to(device)
-    from uutils.torch_uu.models.resnet_rfs import get_resnet_rfs_model_mi
-    model, _ = get_resnet_rfs_model_mi('resnet12_hdb1_mio', num_classes=n_train_cls)
+    # from uutils.torch_uu.models.resnet_rfs import get_resnet_rfs_model_mi
+    # model, _ = get_resnet_rfs_model_mi('resnet12_rfs', num_classes=n_train_cls)
+    from uutils.torch_uu.models.learner_from_opt_as_few_shot_paper import get_default_learner_and_hps_dict
+    args.model, args.model_hps = get_default_learner_and_hps_dict()
+    # - get model
+    model = args.model
+    model.to(device)
+    from torch import nn
     criterion = nn.CrossEntropyLoss()
     for split, dataloader in dataloaders.items():
         print(f'-- {split=}')
@@ -116,8 +121,9 @@ def loop_through_usl_hdb1_and_pass_data_through_mdl():
             print(f'{y=}')
 
             y_pred = model(X)
-            loss = criterion(y_pred, y)
-            print(f'{loss=}')
+            print(f'{y_pred.size()=}')
+            # loss = criterion(y_pred, y)
+            # print(f'{loss=}')
             print()
             break
     print('-- end of test --')
