b0a0b100c83ad7cd6a38d17f177b1af280eb26a9
Saumya Goyal <saumyagoyal01@gmail.com>
2023-08-22 16:45:35 -0700

    Updates to gpt2 files

--- div_src/diversity_src/experiment_mains/main_maml_gpt2.py
+++ div_src/diversity_src/experiment_mains/main_maml_gpt2.py
@@ -17,6 +17,7 @@ from argparse import Namespace
 
 from learn2learn.vision.benchmarks import BenchmarkTasksets
 
+import uutils
 from uutils import args_hardcoded_in_script, report_times
 from uutils.argparse_uu.common import setup_args_for_experiment, setup_wandb
 from uutils.argparse_uu.meta_learning import parse_args_meta_learning, fix_for_backwards_compatibility
@@ -54,96 +55,105 @@ def load_args() -> Namespace:
     # -- parse args from terminal
     # args: Namespace = parse_args_standard_sl()
     args: Namespace = parse_args_meta_learning()
-
-    args.manual_loads_name = 'maml_gpt2_webtext2'
-
     args.model_option = 'gpt2'
-    args.model_hps = dict(block_size = 64, vocab_size = 50257, n_layer = 12, n_head = 12, n_embd = 768, dropout = 0.1)
+    args.manual_loads_name = 'maml_gpt2_webtext2'
+    args.args_hardcoded_in_script = True  # <- REMOVE to remove manual loads
 
     # - data
     from pathlib import Path
     args.data_option = 'webtext'  # no name assumes l2l, make sure you're calling get_l2l_tasksets
     args.data_path = Path('~/data/webtext/').expanduser()
     args.data_augmentation = 'lee2019'
-    args.log_root = '/lfs/hyperion/0/saumg'
-
-    # - training mode
-    args.training_mode = 'iterations'
-
-    args.num_its = 1000
-
-    # - debug flag
-    # args.debug = True
-    args.debug = False
-
-    # - opt
-    args.opt_option = 'Adam_default'
-    args.lr = 6e-4  # match MAML++
-    args.weight_decay = 1e-2
-    args.opt_hps: dict = dict(lr=args.lr, weight_decay = args.weight_decay)
-
-    args.scheduler_option = 'Adam_cosine_scheduler_rfs_cifarfs'
-    args.log_scheduler_freq = 1
-    args.T_max = args.num_its // args.log_scheduler_freq  # intended 800K/2k
-    args.eta_min = 1e-5  # match MAML++
-    args.scheduler_hps: dict = dict(T_max=args.T_max, eta_min=args.eta_min)
-    # assert args.T_max == 400, f'T_max is not expected value, instead it is: {args.T_max=}'
-
-    # -- Meta-Learner
-    # - maml
-    args.meta_learner_name = 'maml_fixed_inner_lr'
-    args.inner_lr = 1e-3  # same as fast_lr in l2l
-    args.nb_inner_train_steps = 5
-    # args.track_higher_grads = True  # set to false only during meta-testing and unofficial fo, but then args.fo has to be True too. Note code sets it automatically only for meta-test
-    args.first_order = True
-    args.k_shots = 1
-    args.k_eval = 1
-    args.n_cls = 4
-
-    # - outer trainer params
-    args.batch_size = 8
-
-    # - dist args
-    args.world_size = torch.cuda.device_count()
-    # args.world_size = 8
-    args.parallel = args.world_size > 1
-    args.seed = 42  # I think this might be important due to how tasksets works.
-    args.dist_option = 'l2l_dist'  # avoid moving to ddp when using l2l
-    # args.init_method = 'tcp://localhost:10001'  # <- this cannot be hardcoded here it HAS to be given as an arg due to how torch.run works
-    # args.init_method = f'tcp://127.0.0.1:{find_free_port()}'  # <- this cannot be hardcoded here it HAS to be given as an arg due to how torch.run works
-    args.init_method = None  # <- this cannot be hardcoded here it HAS to be given as an arg due to how torch.run works
-
-    # -
-    args.log_freq = 100
-
-    # -- wandb args
-    args.wandb_project = 'entire-diversity-spectrum'
-    # - wandb expt args
-    args.experiment_name = f'maml_gpt2_webtext2'
-    args.run_name = f'{args.model_option} {args.opt_option} {args.scheduler_option} {args.lr}: {args.jobid=}'
-    args.log_to_wandb = True
-    args.wandb_entity = 'saumyagoyal01'
-    # args.log_to_wandb = False
-    # args.dir_wandb = Path('/shared/rsaas/miranda9/data/logs/')
-
-    # - fix for backwards compatibility
-    args = fix_for_backwards_compatibility(args)
+    args.log_root = '/dfs/scratch0/saumg/gpt_logs/maml_logs'
 
 
+    if resume_from_checkpoint(args):
+        args: Namespace = make_args_from_supervised_learning_checkpoint(args=args, precedence_to_args_checkpoint=False)
+    else:
+
+        # args.model_hps = dict(block_size = 64, vocab_size = 50257, n_layer = 12, n_head = 12, n_embd = 768, dropout = 0.0)
+        args.model_hps = dict(block_size = 32, vocab_size = 50257, n_layer = 4, n_head = 4, n_embd = 192, dropout = 0.0)
+
+        # - training mode
+        args.training_mode = 'iterations_train_convergence'
+        args.train_convergence_patience = 100000
+
+        args.num_its = 100000
+
+        # - debug flag
+        # args.debug = True
+        args.debug = False
+
+        # - opt
+        # args.opt_option = 'Adam_default'
+        # args.opt_option = 'adamw_gpt'
+        args.opt_option = 'AdafactorDefaultFair'
+        # args.lr = 5e-3
+        args.weight_decay = 1e-2
+        # args.opt_hps: dict = dict(learning_rate=args.lr, weight_decay = args.weight_decay, betas = (0.9, 0.95))
+        # args.opt_hps: dict = dict(lr=args.lr, weight_decay = args.weight_decay)
+
+        # args.scheduler_option = 'None'
+        args.scheduler_option = 'AdafactorSchedule'
+        # args.log_scheduler_freq = 1
+        # args.T_max = args.num_its // args.log_scheduler_freq  # intended 800K/2k
+        # args.eta_min = 1e-5  # match MAML++
+        # args.scheduler_hps: dict = dict(T_max=args.T_max, eta_min=args.eta_min)
+        # assert args.T_max == 400, f'T_max is not expected value, instead it is: {args.T_max=}'
+
+        # -- Meta-Learner
+        # - maml
+        args.meta_learner_name = 'maml_fixed_inner_lr'
+        args.inner_lr = 1e-3  # same as fast_lr in l2l
+        args.nb_inner_train_steps = 5
+        # args.track_higher_grads = True  # set to false only during meta-testing and unofficial fo, but then args.fo has to be True too. Note code sets it automatically only for meta-test
+        args.first_order = True
+        args.k_shots = 1
+        args.k_eval = 1
+        args.n_cls = 4
+
+        # - outer trainer params
+        # args.batch_size = 8
+
+        # - dist args
+        args.world_size = torch.cuda.device_count()
+        # args.world_size = 8
+        args.parallel = args.world_size > 1
+        args.seed = 42  # I think this might be important due to how tasksets works.
+        args.dist_option = 'l2l_dist'  # avoid moving to ddp when using l2l
+        # args.init_method = 'tcp://localhost:10001'  # <- this cannot be hardcoded here it HAS to be given as an arg due to how torch.run works
+        # args.init_method = f'tcp://127.0.0.1:{find_free_port()}'  # <- this cannot be hardcoded here it HAS to be given as an arg due to how torch.run works
+        args.init_method = None  # <- this cannot be hardcoded here it HAS to be given as an arg due to how torch.run works
+
+        # -
+        args.log_freq = 100
+        args.ckpt_freq = 500
+
+        # -- wandb args
+        args.wandb_project = 'entire-diversity-spectrum'
+        # - wandb expt args
+        args.experiment_name = f'maml_gpt2_adafactor'
+        args.run_name = f'{args.model_option} {args.opt_option} {args.scheduler_option} {args.lr}: {args.jobid=}'
+        args.log_to_wandb = True
+        args.wandb_entity = 'saumyagoyal01'
+        # args.log_to_wandb = False
+        # args.dir_wandb = Path('/shared/rsaas/miranda9/data/logs/')
+
+        # - fix for backwards compatibility
+        args = fix_for_backwards_compatibility(args)
 
 
-
-    args.args_hardcoded_in_script = True  # <- REMOVE to remove manual loads
-    # args.manual_loads_name = 'l2l_resnet12rfs_hdb1_100k'  # <- REMOVE to remove manual loads
-    # args.manual_loads_name = 'l2l_resnet12rfs_mi_rfs_adam_cl_100k'  # <- REMOVE to remove manual loads
+        # args.manual_loads_name = 'l2l_resnet12rfs_hdb1_100k'  # <- REMOVE to remove manual loads
+        # args.manual_loads_name = 'l2l_resnet12rfs_mi_rfs_adam_cl_100k'  # <- REMOVE to remove manual loads
 
     # -- set remaining args values (e.g. hardcoded, checkpoint etc.)
     print(f'{args.manual_loads_name=}')
-    if resume_from_checkpoint(args):
-        args: Namespace = make_args_from_supervised_learning_checkpoint(args=args, precedence_to_args_checkpoint=True)
+
+    [print(f'{k, v}') for k, v in vars(args).items()]
     
     # -- Setup up remaining stuff for experiment
     args: Namespace = setup_args_for_experiment(args)
+    args.best_val_loss = 0
     return args
 
 
@@ -154,6 +164,11 @@ def main():
     """
     # - load the args from either terminal, ckpt, manual etc.
     args: Namespace = load_args()
+
+    if resume_from_checkpoint(args) and 'eval' in args.training_mode:
+        print("calling eval...")
+        eval(args)
+        return
 
     # - parallel train
     if not args.parallel:  # serial
@@ -190,6 +205,11 @@ def train(args):
 
     # create the model, opt & scheduler
     get_and_create_model_opt_scheduler_for_run(args)
+    if args.opt_option == 'adamw_gpt' and args.scheduler_option == 'None':
+        # Use custom scheduler
+        args.scheduler_hps: dict = dict(warmup_iters = 2000,
+                lr_decay_iters = 600000, # should be ~= max_iters per Chinchilla
+                min_lr = 6e-5) # minimum learning rate, should be ~= learning_rate/10 per Chinchilla)
     args.number_of_trainable_parameters = count_number_of_parameters(args.model)
     args.opt = move_opt_to_cherry_opt_and_sync_params(args) if is_running_parallel(args.rank) else args.opt
 
@@ -227,6 +247,52 @@ def train(args):
     # cleanup_wandb(args, delete_wandb_dir=True)
     cleanup_wandb(args, delete_wandb_dir=False)
 
+def eval(args):
+    # dont worry about scheduler, since we are not training
+    get_and_create_model_opt_scheduler_for_run(args)
+    args.number_of_trainable_parameters = count_number_of_parameters(args.model)
+    args.opt = move_opt_to_cherry_opt_and_sync_params(args) if is_running_parallel(args.rank) else args.opt
+    
+    # same dataloader for maml and usl
+    args.dataloaders: dict = get_sl_dataloader(args)
+
+    # use meta learner for gpt2 eval
+    args.meta_learner_name = 'maml_fixed_inner_lr'
+    args.inner_lr = 1e-3  # same as fast_lr in l2l
+    args.nb_inner_train_steps = 10
+    # args.track_higher_grads = True  # set to false only during meta-testing and unofficial fo, but then args.fo has to be True too. Note code sets it automatically only for meta-test
+    args.first_order = True
+    args.agent: Agent = GPTMetaLearnerL2L(args, args.model)
+
+    args.bar = uutils.get_good_progressbar(max_value=100)
+
+    args.agent.eval()
+
+    val_accs = []
+    val_losses = []
+
+    [print(f'{k, v}') for k, v in vars(args).items()]
+
+    for i, batch in enumerate(args.dataloaders['val']):
+        batch = (batch[0].to(args.device), batch[1].to(args.device))
+
+        # print("calling eval_forward")
+        val_loss, val_loss_ci, val_acc, val_acc_ci = args.agent.eval_forward(batch, training = False)
+        print(val_loss, val_loss_ci, val_acc, val_acc_ci)
+        # print("back")
+        val_losses.append(val_loss.item())
+        val_accs.append(val_acc.item())
+
+        args.bar.update(i+1)
+        if i+1 == 100:
+            break
+
+    import statistics
+    val_loss_mean = statistics.mean(val_losses)
+    val_acc_mean = statistics.mean(val_accs)
+
+    print("Results...")
+    print(f'{val_loss_mean=}, {val_acc_mean=}')
 
 # -- Run experiment
 
--- div_src/diversity_src/experiment_mains/main_usl_gpt2.py
+++ div_src/diversity_src/experiment_mains/main_usl_gpt2.py
@@ -5,6 +5,7 @@ import torch.multiprocessing as mp
 
 from argparse import Namespace
 
+import uutils
 from uutils import args_hardcoded_in_script, report_times
 from uutils.argparse_uu.common import setup_args_for_experiment
 from uutils.argparse_uu.supervised_learning import make_args_from_supervised_learning_checkpoint, parse_args_standard_sl
@@ -13,10 +14,11 @@ from uutils.argparse_uu.supervised_learning import make_args_from_supervised_lea
 from uutils.torch_uu import count_number_of_parameters
 from uutils.torch_uu.agents.common import Agent
 from uutils.torch_uu.agents.supervised_learning import ClassificationSLAgent, UnionClsSLAgent, GPT2SLAgent
+from uutils.torch_uu.meta_learners.gpt2_meta_learner import GPTMetaLearnerL2L
 from uutils.torch_uu.checkpointing_uu import resume_from_checkpoint
 from uutils.torch_uu.dataloaders.helpers import get_sl_dataloader
 from uutils.torch_uu.distributed import set_sharing_strategy, print_process_info, set_devices, setup_process, cleanup, \
-    print_dist
+    print_dist, is_running_parallel
 # from uutils.torch_uu.mains.common import get_and_create_model_opt_scheduler_first_time
 # from uutils.torch_uu.training.supervised_learning import train_agent_fit_single_batch, train_agent_iterations, \
 #     train_agent_epochs
@@ -57,7 +59,7 @@ def load_args() -> Namespace:
 
     # -- set remaining args values (e.g. hardcoded, checkpoint etc.)
     if resume_from_checkpoint(args):
-        args: Namespace = make_args_from_supervised_learning_checkpoint(args=args, precedence_to_args_checkpoint=True)
+        args: Namespace = make_args_from_supervised_learning_checkpoint(args=args, precedence_to_args_checkpoint=False)
     elif args_hardcoded_in_script(args):
         from pathlib import Path
         # - model
@@ -66,20 +68,33 @@ def load_args() -> Namespace:
         #                       levels=None,
         #                       spp=False, in_channels=3)
 
-        args.model_hps = dict(block_size = 64, vocab_size = 50257, n_layer = 12, n_head = 12, n_embd = 768, dropout = 0.0)
+        # args.model_hps = dict(block_size = 64, vocab_size = 50257, n_layer = 12, n_head = 12, n_embd = 768, dropout = 0.0)
+        args.model_hps = dict(block_size = 32, vocab_size = 50257, n_layer = 4, n_head = 4, n_embd = 192, dropout = 0.0)
 
         # - data
         args.data_option = 'webtext'
         args.data_path = Path('~/data/webtext/').expanduser()
+        args.log_root = '/lfs/hyperion/0/saumg/logs/usl_gpt2/'
 
         # - opt
-        args.opt_option = 'adamw_gpt'
-        args.num_its = 1000
-        args.lr = 6e-4
+        # args.opt_option = 'adamw_gpt'
+        # args.opt_option = 'Adam_default'
+        args.opt_option = 'AdafactorDefaultFair'
+        # args.opt_option = 'sophia'
+        args.num_its = 100000
+        # # Sophia
+        # args.lr = 3e-4
+        # args.weight_decay = 1e-1
+        # args.opt_hps = dict(lr = args.lr, weight_decay = args.weight_decay, betas = (0.9,0.95), rho = 0.1)
+        # Adafactor
         args.weight_decay = 1e-2
-        args.opt_hps: dict = dict(learning_rate=args.lr, weight_decay = args.weight_decay, betas = (0.9, 0.95))
+        # # Adamw
+        # args.lr = 1e-3
+        # args.weight_decay = 1e-2
+        # args.opt_hps: dict = dict(learning_rate=args.lr, weight_decay = args.weight_decay, betas = (0.9, 0.95))
 
-        args.scheduler_option = 'None'
+        # args.scheduler_option = 'None'
+        args.scheduler_option = 'AdafactorSchedule'
         # args.log_scheduler_freq = 1
         # args.T_max = args.num_its // args.log_scheduler_freq
         # args.eta_min = 1e-5  # coincidentally, matches MAML++
@@ -87,7 +102,8 @@ def load_args() -> Namespace:
 
 
         # - training mode
-        args.training_mode = 'iterations'
+        args.training_mode = 'iterations_train_convergence'
+        args.train_convergence_patience = 100000
 
         # -
         # args.debug = True
@@ -95,11 +111,12 @@ def load_args() -> Namespace:
 
         # -
         args.log_freq = 100  # for SL it is meant to be small e.g. 1 or 2
+        args.ckpt_freq = 500
 
         # - wandb args
         args.wandb_project = 'entire-diversity-spectrum'
         # - wandb expt args
-        args.experiment_name = f'usl_gpt2_webtext2'
+        args.experiment_name = f'usl_gpt2_adafactor_init'
         args.run_name = f'{args.model_option} {args.opt_option} {args.scheduler_option} {args.lr}: {args.jobid=} {gethostname()}'
         args.log_to_wandb = True
         args.wandb_entity = 'saumyagoyal01'
@@ -118,6 +135,14 @@ def main():
     """
     # - load the args from either terminal, ckpt, manual etc.
     args: Namespace = load_args()
+
+    
+
+    if resume_from_checkpoint(args) and 'eval' in args.training_mode:
+        print("calling eval...")
+        eval(args)
+        return
+
     [print(f'{k, v}') for k, v in vars(args).items()]
 
     # - parallel train
@@ -143,9 +168,11 @@ def train(rank, args):
 
     # create the (ddp) model, opt & scheduler
     get_and_create_model_opt_scheduler_for_run(args)
-    args.scheduler_hps: dict = dict(warmup_iters = 2000,
-            lr_decay_iters = 600000, # should be ~= max_iters per Chinchilla
-            min_lr = 6e-5) # minimum learning rate, should be ~= learning_rate/10 per Chinchilla)
+    if args.opt_option == 'adamw_gpt' and args.scheduler_option == 'None':
+        # Use custom scheduler
+        args.scheduler_hps: dict = dict(warmup_iters = 2000,
+                lr_decay_iters = 600000, # should be ~= max_iters per Chinchilla
+                min_lr = 6e-5) # minimum learning rate, should be ~= learning_rate/10 per Chinchilla)
     args.number_of_trainable_parameters = count_number_of_parameters(args.model)
 
     # create the dataloaders, this goes first so you can select the mdl (e.g. final layer) based on task
@@ -170,7 +197,7 @@ def train(rank, args):
         # note train code will see training mode to determine halting criterion
         if halt_loss == 'val':
             train_agent_iterations(args, args.agent, args.dataloaders, args.opt, args.scheduler, halt_loss = 'val', target_loss = 3.039)
-        train_agent_iterations(args, args.agent, args.dataloaders, args.opt, args.scheduler)
+        train_agent_iterations(args, args.agent, args.dataloaders, args.opt, args.scheduler, use_half_loss = False)
     elif 'epochs' in args.training_mode:
         # note train code will see training mode to determine halting criterion
         train_agent_epochs(args, args.agent, args.dataloaders, args.opt, args.scheduler)
@@ -183,6 +210,56 @@ def train(rank, args):
     print(f'\n----> about to cleanup worker with rank {rank}')
     cleanup(rank)
     print(f'clean up done successfully! {rank}')
+
+def eval(args):
+    # dont worry about scheduler, since we are not training
+    get_and_create_model_opt_scheduler_for_run(args)
+    args.number_of_trainable_parameters = count_number_of_parameters(args.model)
+    args.opt = move_opt_to_cherry_opt_and_sync_params(args) if is_running_parallel(args.rank) else args.opt
+    
+    # same dataloader for maml and usl
+    args.dataloaders: dict = get_sl_dataloader(args)
+
+    # use meta learner for gpt2 eval
+    args.meta_learner_name = 'maml_fixed_inner_lr'
+    args.inner_lr = 1e-3  # same as fast_lr in l2l
+    args.nb_inner_train_steps = 5
+    # args.track_higher_grads = True  # set to false only during meta-testing and unofficial fo, but then args.fo has to be True too. Note code sets it automatically only for meta-test
+    args.first_order = True
+    args.agent: Agent = GPTMetaLearnerL2L(args, args.model)
+
+    args.bar = uutils.get_good_progressbar(max_value=100)
+
+    args.agent.eval()
+
+    val_accs = []
+    val_losses = []
+
+    [print(f'{k, v}') for k, v in vars(args).items()]
+
+    for i, batch in enumerate(args.dataloaders['val']):
+        batch = (batch[0].to(args.device), batch[1].to(args.device))
+
+        # print("calling eval_forward")
+        val_loss, val_loss_ci, val_acc, val_acc_ci = args.agent.eval_forward(batch, training = False)
+        print(val_loss, val_loss_ci, val_acc, val_acc_ci)
+        # print("back")
+        val_losses.append(val_loss.item())
+        val_accs.append(val_acc.item())
+
+        args.bar.update(i+1)
+        if i+1 == 100:
+            break
+
+    import statistics
+    val_loss_mean = statistics.mean(val_losses)
+    val_acc_mean = statistics.mean(val_accs)
+
+    print("Results...")
+    print(f'{val_loss_mean=}, {val_acc_mean=}')
+
+
+
 
 
 if __name__ == "__main__":
